590574d3fd24b9baa51c73dbc41c33eeefef1273
meticulousdev <meticulousdev@gmail.com>
2024-12-09 21:30:23 +0900

    querySelector

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const hellos = document.getElementsByClassName("hello");
+const title = document.querySelector(".hello h1");
+// const title = document.getElementsByClassName("hello");
+// const title = document.getElementsByTagName("h1");
 
-console.log(hellos)
+console.log(title)
