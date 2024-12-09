b29330a068eb4ec62c08f7f443f274f0d8fb8750
meticulousdev <meticulousdev@gmail.com>
2024-12-09 21:38:43 +0900

    TODO list update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const title = document.querySelector(".hello h1");
+
+// const title = document.querySelector("#hello");
 // const title = document.getElementsByClassName("hello");
 // const title = document.getElementsByTagName("h1");
 
-console.log(title)
+// console.log(title)
